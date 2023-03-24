554f774c055269f5e932aae2641e1e7a408db542
Sam <98746718+Sam-L-White@users.noreply.github.com>
2023-03-24 15:21:19 +0000

    Add insert function, add pretty print function

--- script.js
+++ script.js
@@ -14,10 +14,44 @@ function buildTree(array, startIndex, endIndex){
 }
 
 const tree = (array) => {
+
     let root = buildTree(array, 0, array.length - 1)
-    return{get root(){return root}}
+
+    const insert = (input, currentRoot = root) => {
+        if(input < currentRoot.data){
+            if(currentRoot.left !== null){
+                insert(input, currentRoot.left)
+            } else {
+                currentRoot.left = node(input)
+            } 
+        } else {
+            if(currentRoot.right !== null){
+                insert(input, currentRoot.right)
+            } else {
+                currentRoot.right = node(input)
+            } 
+        }
+    }
+    return{get root(){return root}, insert}
 }
 
-let array = [1,2,3,4,5]
-console.log(tree(array).root)
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+    if (node === null) {
+       return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+}
 
+let array = [1,2,4,5,9,10,14]
+let treeResult = (tree(array))
+
+prettyPrint(treeResult.root)
+treeResult.insert(3)
+treeResult.insert(11)
+prettyPrint(treeResult.root)
