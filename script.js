84973b47b18fdda32d866597a220f7fa0e74b771
Sam <98746718+Sam-L-White@users.noreply.github.com>
2023-03-24 14:56:06 +0000

    Add binary tree creation

---
+++ script.js
@@ -0,0 +1,23 @@
+const node = (data, left = null, right = null) => {
+    return{data, left, right}
+}
+
+function buildTree(array, startIndex, endIndex){
+    if(startIndex > endIndex){
+        return null
+    }
+    let midIndex = Math.floor((startIndex + endIndex) / 2)
+    let root = node(array[midIndex])
+    root.left = buildTree(array, startIndex, midIndex - 1)
+    root.right = buildTree(array, midIndex + 1, endIndex)
+    return root
+}
+
+const tree = (array) => {
+    let root = buildTree(array, 0, array.length - 1)
+    return{get root(){return root}}
+}
+
+let array = [1,2,3,4,5]
+console.log(tree(array).root)
+
